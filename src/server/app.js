7ccd9cd55435fb1157bb5a20cebf6523eed945ec
Dublin Anondson <stratophone2112@gmail.com>
2019-11-04 19:12:09 -0600

    feat: added top artists
    next: break out sections into separate panels
    next: refactor js code to not be one big IIFE

--- src/server/app.js
+++ src/server/app.js
@@ -23,7 +23,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  const scope = 'user-read-private user-read-email user-read-recently-played';
+  const scope = 'user-read-private user-read-email user-read-recently-played user-top-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
